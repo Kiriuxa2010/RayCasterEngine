5dc95dfa3810027463126167ead6f31041c9ff87
Kiriuxa2010 <71524929+Kiriuxa2010@users.noreply.github.com>
2022-07-13 12:00:09 -0700

    Pseudo3d Engine made in Python
    A Pseudo3d Raycaster Engine made in Python

---
+++ Game1/map.py
@@ -0,0 +1,18 @@
+from settings import *
+
+text_map = [
+    'WWWWWWWWWWWWWW',
+    'W..W...W.....W',
+    'W......www...W',
+    'W....W..WW...W',
+    'W..W....W....W',
+    'W..W...W.W...W',
+    'W....W.......W',
+    'WWWWWWWWWWWWWW',
+]
+
+world_map = set()
+for j, row in enumerate(text_map):
+    for i, char in enumerate(row):
+        if char == 'W':
+            world_map.add((i * TILE, j * TILE))
---
+++ README.md
@@ -0,0 +1,4 @@
+This is A Pseudo3d RayCaster Engine Made in Python.
+You can use it for your goals.
+
+Original creator Kirill Curachin.
